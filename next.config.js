887436045c327db1ef232783af2d9eb7a91957fe
Ubuntu <ubuntu@vps-1a918503.vps.ovh.ca>
2026-02-16 08:21:30 +0000

    chore: add GitHub Pages config

--- next.config.js
+++ next.config.js
@@ -1,8 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
+  basePath: '/focus-website',
   images: {
     unoptimized: true,
   },
+  trailingSlash: true,
 };
 
 module.exports = nextConfig;
